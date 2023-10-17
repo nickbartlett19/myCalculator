1cf8710f4bd2ea996902559744de42413bf16cd0
Nick Bartlett <nickbartlett19@gmail.com>
2023-10-17 12:18:51 -0400

    add button functionality, styling

--- script.js
+++ script.js
@@ -3,30 +3,42 @@
 // when it is clicked.
 
 
-const equalsBtn = document.querySelector('[equals-button]');
+const equalsBtn = document.querySelector('[equals]');
+const clearBtn = document.querySelector('[clear]')
 const screen = document.querySelector('[screen]');
 const numberBtn = document.querySelectorAll('[number]');
+const addBtn = document.querySelector('[add]');
+const subtractBtn = document.querySelector('[subtract]');
+const multiplyBtn = document.querySelector('[multiply]');
+const divideBtn = document.querySelector('[divide]');
+const negateBtn = document.querySelector('[negate]');
+const percentBtn = document.querySelector('[percent]');
+const decimalBtn = document.querySelector('[decimal]');
+
 // console.log(equalsBtn.innerHTML);
 // console.log(equalsBtn);
 
 equalsBtn.addEventListener('click', (e)=> {
     // equalsBtn.style.color = 'blue';
-    // console.log("Wu - it works!");
+    console.log("Wu - it works!");
 })
 
-numberBtn.forEach(numberBtn => {
-    addEventListener('click', (e)=> {
-        console.log(numberBtn.innerHTML)
-        // screen.innerHTML = button.innerHTML;
-    })
-})
-
-console.log(numberBtn.length);
 for (let i = 0; i < numberBtn.length; i++) {
     numberBtn[i].addEventListener('click', (e)=> {
-        console.log()
-        console.log(numberBtn[i].innerHTML)
-        // screen.innerHTML = button.innerHTML;
+        screen.innerHTML = screen.innerHTML + numberBtn[i].innerHTML;
+        // console.log(numberBtn[i].innerHTML)
     })
-    console.log("added eventlistener to ", numberBtn[i].innerHTML);
 }
+
+clearBtn.addEventListener('click', (e) => {
+    screen.innerHTML = '';
+})
+
+decimalBtn.addEventListener('click', (e) => {
+    let str = screen.innerHTML;
+
+    if (str.includes('.') == false) {
+        screen.innerHTML = screen.innerHTML + decimalBtn.innerHTML;
+    }
+})
+
