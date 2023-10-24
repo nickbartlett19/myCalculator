1257276bab85436bd3bce5e787767afb96c74ade
Nick Bartlett <nickbartlett19@gmail.com>
2023-10-24 12:56:00 -0400

    add crude functionality, debug

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 
 
 const equalsBtn = document.querySelector('[equals]');
-const clearBtn = document.querySelector('[clear]')
-const screen = document.querySelector('[screen]');
 const numberBtn = document.querySelectorAll('[number]');
+const clearBtn = document.querySelector('[clear]')
+
+// top row, screen
+const pastOperand = document.querySelector('[pastOperandBox]');
+const operator = document.querySelector('[operatorBox]');
+const presOperand = document.querySelector('[presOperandBox]');
+
 const addBtn = document.querySelector('[add]');
 const subtractBtn = document.querySelector('[subtract]');
 const multiplyBtn = document.querySelector('[multiply]');
@@ -19,26 +24,104 @@ const decimalBtn = document.querySelector('[decimal]');
 // console.log(equalsBtn);
 
 equalsBtn.addEventListener('click', (e)=> {
-    // equalsBtn.style.color = 'blue';
-    console.log("Wu - it works!");
+    // console.log("Wu - it works!");
+
+    console.log(operator.innerHTML);
+    const currOperator = decipherOperator(operator.innerHTML);
+    console.log(currOperator);
+
+    if (currOperator !== false) {
+        var newPresOperand = '';
+        // newPresOperand = Number(newPresOperand);
+        switch (currOperator) {
+            case("divide"):
+                newPresOperand = Number(pastOperand.innerHTML) / Number(presOperand.innerHTML);
+                break;
+            case("multiply"):
+                newPresOperand = Number(pastOperand.innerHTML) * Number(presOperand.innerHTML);
+                break;
+            case("subtract"):
+                newPresOperand = Number(pastOperand.innerHTML) - Number(presOperand.innerHTML);
+                break;
+            case("add"):
+                console.log(pastOperand.innerHTML);
+                newPresOperand = Number(pastOperand.innerHTML) + Number(presOperand.innerHTML);
+                break;
+            default:
+                console.log("error with operator");            
+        }
+    }
+    console.log(newPresOperand);
+    pastOperand.innerHTML = '';
+    operator.innerHTML = '';
+    presOperand.innerHTML = '';
+    presOperand.innerHTML = newPresOperand; 
+
 })
 
 for (let i = 0; i < numberBtn.length; i++) {
     numberBtn[i].addEventListener('click', (e)=> {
-        screen.innerHTML = screen.innerHTML + numberBtn[i].innerHTML;
+        presOperand.innerHTML = presOperand.innerHTML + numberBtn[i].innerHTML;
         // console.log(numberBtn[i].innerHTML)
     })
 }
 
 clearBtn.addEventListener('click', (e) => {
-    screen.innerHTML = '';
+    pastOperand.innerHTML = '';
+    operator.innerHTML = '';
+    presOperand.innerHTML = '';
 })
 
 decimalBtn.addEventListener('click', (e) => {
-    let str = screen.innerHTML;
+    let str = presOperand.innerHTML;
 
     if (str.includes('.') == false) {
-        screen.innerHTML = screen.innerHTML + decimalBtn.innerHTML;
+        presOperand.innerHTML = presOperand.innerHTML + decimalBtn.innerHTML;
     }
 })
 
+// operator buttons
+
+addBtn.addEventListener('click', (e) => {
+    operator.innerHTML = "+";
+    pastOperand.innerHTML = presOperand.innerHTML;
+    presOperand.innerHTML = '';
+
+})
+
+subtractBtn.addEventListener('click', (e) => {
+    operator.innerHTML = "-";
+    pastOperand.innerHTML = presOperand.innerHTML;
+    presOperand.innerHTML = '';
+})
+
+multiplyBtn.addEventListener('click', (e) => {
+    operator.innerHTML = "&#xD7;";
+    pastOperand.innerHTML = presOperand.innerHTML;
+    presOperand.innerHTML = '';
+})
+
+divideBtn.addEventListener('click', (e) => {
+    operator.innerHTML = "÷";
+    pastOperand.innerHTML = presOperand.innerHTML;
+    presOperand.innerHTML = '';
+})
+
+
+function decipherOperator(operator) {
+    if (operator === "÷") {
+        return "divide";
+    }
+    else if (operator === "×") {
+        return "multiply";
+    }
+    else if (operator === "-") {
+        return "subtract";
+    }
+    else if (operator === "+") {
+        return "add";
+    }
+    else {
+        return false;
+    }
+}
